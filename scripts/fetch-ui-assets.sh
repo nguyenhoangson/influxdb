f0a99da141c6bdab0d39eb0f509e3f6010b0494d
Will Baker <wbaker@gmail.com>
2021-10-13 16:16:31 -0600

    build: update ci related to UI and e2e tests

--- scripts/fetch-ui-assets.sh
+++ scripts/fetch-ui-assets.sh
@@ -18,20 +18,18 @@ declare -r SCRIPT_DIR=$(cd $(dirname ${0}) >/dev/null 2>&1 && pwd)
 declare -r ROOT_DIR=$(dirname ${SCRIPT_DIR})
 declare -r STATIC_DIR="$ROOT_DIR/static"
 
-# Download the SHA256 checksum attached to the release. To verify the integrity
-# of the download, this checksum will be used to check the download tar file
-# containing the built UI assets.
-curl -Ls https://github.com/influxdata/ui/releases/download/OSS-Master/sha256.txt --output sha256.txt
+# This must be updated depending on the actual sha256 checksum of the tar file
+# attached to the release.
+EXPECTED_UI_CHECKSUM=a3a138ad25edb9ea8517a4598da0a4b477d840c15a041e6447851acbf95ac010
 
 # Download the tar file containing the built UI assets.
 curl -L https://github.com/influxdata/ui/releases/download/OSS-Master/build.tar.gz --output build.tar.gz
 
 # Verify the checksums match; exit if they don't.
-echo "$(cat sha256.txt)" | sha256sum --check -- \
+echo "${EXPECTED_UI_CHECKSUM} build.tar.gz" | sha256sum --check -- \
     || { echo "Checksums did not match for downloaded UI assets!"; exit 1; }
 
 # Extract the assets and clean up.
 mkdir -p "$STATIC_DIR/data"
 tar -xzf build.tar.gz -C "$STATIC_DIR/data"
-rm sha256.txt
 rm build.tar.gz
